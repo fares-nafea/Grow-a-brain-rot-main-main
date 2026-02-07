f13909b1967f0e7d0012b9dac6f82711222fff9e
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-02-07 23:34:01 +0200

    Update PlotService to set player thumbnail image and adjust transparency, enhancing visual representation on signs.

--- src/server/Services/PlotService.lua
+++ src/server/Services/PlotService.lua
@@ -11,7 +11,14 @@ local Plots = Map:WaitForChild("Plots")
 function Service.getPlots()
     return Plots:GetChildren()
 end
-
+function Service.getPlot(player: Player)
+    for _, plot: Model in workspace.World.Map.Plots:GetChildren() do
+        if plot:GetAttribute("Taken") == true and plot :GetAttribute("Owner") == player.UserId then
+            return plot
+        end
+    end
+    return nil
+end
 function Service.getAvailablePlot(player: Player)
     for _, plot: Model in ipairs(Service.getPlots()) do
         if plot:GetAttribute("Taken") then
@@ -21,7 +28,6 @@ function Service.getAvailablePlot(player: Player)
     end
     return nil
 end
-
 function Service.dataLoaded(player: Player)
     local plot = Service.getAvailablePlot(player)
     if plot then
@@ -33,10 +39,22 @@ function Service.dataLoaded(player: Player)
         
         local ImageSize = Enum.ThumbnailSize.Size420x420
         local ImageType = Enum.ThumbnailType.HeadShot
- 
         local content = players:GetUserThumbnailAsync(player.UserId, ImageType, ImageSize)
+        playerSign.Main.SurfaceGui.ImageLabel.ImageTransparency = 0
+        playerSign.Main.SurfaceGui.ImageLabel.Image = content
     end
 end
+function Service.playerRemoved(player: Player)
+    local foundPlot = Service.getPlot(player)
+    if foundPlot then
+        foundPlot:SetAttribute("Taken", nil)
+        foundPlot:SetAttribute("Owner", nil)
 
+        local playerSign: Model = foundPlot.PlayerSign
+        playerSign.Main.SurfaceGui.TextLabel.Text = "Empty Garden"
+        playerSign.Main.SurfaceGui.ImageLabel.ImageTransparency = 1
+        playerSign.Main.SurfaceGui.ImageLabel.Image = ""
+    end
+end
 
 return Service
