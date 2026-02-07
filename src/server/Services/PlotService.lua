7a7bff4e68c8feb4b95e5ee771029edaee15c0cc
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-02-07 21:55:10 +0200

    Refactor PlotService to improve plot retrieval and ownership assignment; replace getMaxPlots with getPlots for better clarity and efficiency, and update player sign display with user thumbnail.

--- src/server/Services/PlotService.lua
+++ src/server/Services/PlotService.lua
@@ -1,28 +1,40 @@
--- Plot Service
+-- PlotService
 
+local players = game:GetService("Players")
 
 local Service = {}
 
-function Service.getMaxPlots()
-    return workspace.World.Map.Plots:GetChildren()
+local World = workspace:WaitForChild("World")
+local Map = World:WaitForChild("Map")
+local Plots = Map:WaitForChild("Plots")
+
+function Service.getPlots()
+    return Plots:GetChildren()
 end
+
 function Service.getAvailablePlot(player: Player)
-    for i = 1, Service.getMaxPlots() do
-        local correspondingPlot: Model = workspace.World.Map.Plots[tostring(i)]
-        if correspondingPlot:GetAttribute("Taken") == true then
+    for _, plot: Model in ipairs(Service.getPlots()) do
+        if plot:GetAttribute("Taken") then
             continue
         end
-        return correspondingPlot
+        return plot
     end
     return nil
 end
+
 function Service.dataLoaded(player: Player)
     local plot = Service.getAvailablePlot(player)
     if plot then
         plot:SetAttribute("Taken", true)
         plot:SetAttribute("Owner", player.UserId)
 
-        print(player.Name, plot, plot.Name)
+        local playerSign: Model = plot.PlayerSign
+        playerSign.Main.TextLabel.Text = game.Name
+
+        local ImageSize = Enum.ThumbnailSize.Size420x420
+        local ImageType = Enum.ThumbnailType.HeadShot
+ 
+        local content = players:GetUesrThumbnailAsync(player.UserId, ImageType, ImageSize)
     end
 end
 
