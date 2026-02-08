b8d9b4e9b105ad4d920b02fa3c27eba2dcfe9120
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-02-08 18:31:20 +0200

    Refactor buttonTPs.client.lua to streamline teleportation logic and improve GUI element handling; removed unnecessary checks and enhanced character teleportation method for better performance.

--- src/client/buttonTPs.client.lua
+++ src/client/buttonTPs.client.lua
@@ -1,27 +1,25 @@
 -- Button TPs
 
 local Players = game:GetService("Players")
+
 local player = Players.LocalPlayer
-
-local playerGui = player:WaitForChild("PlayerGui")
-
-local gui = script.Parent
-local root = gui:WaitForChild("Root", 5)
-if not root then return end  -- لو Root مش موجود بعد 5 ثواني، يوقف السكريبت
-
-local gardenTPFrame = root:WaitForChild("GardenTP", 5)
-if not gardenTPFrame then return end
-
-repeat task.wait() until player:GetAttribute("DataLoaded") == true
-
 local character = player.Character or player.CharacterAdded:Wait()
 local rootPart: BasePart = character:WaitForChild("HumanoidRootPart")
 
+local gui = script.Parent
+
+local root = gui:WaitForChild("Root", math.huge)
+local gardenTPFrame = root:WaitForChild("GardenTP")
+local seedsTPFrame = root:WaitForChild("SeedsTP")
+local sellTPFrame = root:WaitForChild("SellTP")
+
+repeat task.wait() until player:GetAttribute("DataLoaded") == true
+
 gardenTPFrame.Button.MouseButton1Click:Connect(function()
-    for _, plot: Model in ipairs(workspace.World.Map.Plots:GetChildren()) do
+    for _, plot: Model in workspace.World.Map.Plots:GetChildren() do
         if plot:GetAttribute("Taken") == true and plot:GetAttribute("Owner") == player.UserId then
-            local tpPart: BasePart = plot:WaitForChild("TPpart")
-            rootPart.CFrame = tpPart.CFrame
+            -- teleport here
+            character:SetPrimaryPartCFrame(plot.TPPart.CFrame)
             return
         end
     end
