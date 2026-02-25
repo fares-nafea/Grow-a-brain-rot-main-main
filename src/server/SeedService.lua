8388328db6ffacae2e7a73783a982475f46085c7
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-02-25 20:55:14 +0200

    Add seed purchasing functionality to SeedService; implement giveSeed method for inventory management and integrate remote event handling for seed transactions, enhancing player interaction with seed stocks.

--- src/server/SeedService.lua
+++ src/server/SeedService.lua
@@ -3,15 +3,42 @@
 local replicatedStorage = game:GetService("ReplicatedStorage")
 local players = game:GetService("Players")
 
+local remotes = replicatedStorage.Remotes
 local modules = replicatedStorage.Modules
 local serverInfo = replicatedStorage.ServerInfo
 
 local seedDataModules = require(modules.SeedData)
 
 local Service = {
+    cachedModules = {},
     DEFAULT_RESTOCK_TIME = 10,
 }
 
+function Service.giveSeed(player: Player, seedName: string, amount: number, reduceStock: boolean)
+    if player and seedName then
+        -- Give the [player] the [seedName]
+        local seedData = seedDataModules.getData(seedName)
+        local playerData = Service.cachedModules.DataService.getData(player)
+        if playerData and seedData then
+            local inventory = playerData.Inventory
+            local foundSeed = inventory[seedName]
+
+            if reduceStock == true then
+                -- Reduce
+                seedData.Server.CurrentStock.Value = math.clamp(
+                    seedData.Server.CurrentStock.Value-amount,
+                    0,
+                    seedData.Server.MaxStock.Value
+                )
+            end
+            if foundSeed then
+                foundSeed.Count += amount
+            else
+                inventory[seedName] = {Count = amount}
+            end
+        end
+    end
+end
 function Service.restockSeed(data: any)
     if data then
         if data.resetTimer then
@@ -34,6 +61,30 @@ function Service.restockSeed(data: any)
     ---
 end
 function Service.init()
+    local dataService = Service.cachedModules.DataService
+    local moneyService = Service.cachedModules.MoneyService
+    -- Handling Remote Events
+    remotes.BuySeed.onServerEvent:Connect(function(player, seedName: string)
+        if player:GetAttribute("DataLoaded") ~= true then return end
+
+        local playerData = dataService.getData(player)
+        if not playerData then return end
+
+        local seedData = seedDataModules.getData(seedName)
+        if seedData then
+            -- Checking if inStock
+            if seedData.Server.CurrentStock.Value <= 0 then
+                return
+            end
+            --- Checking if has enough money
+            if playerData.Sheckles < seedData.Cost.Value then
+                return
+            end
+            -- Buy the Seed
+            moneyService.giveMoney(player, - seedData.Cost.Value)
+            Service.giveSeed(player, seedName, 1, true)
+        end
+    end)
     -- Restocking Timer
     serverInfo.SEED_RESTOCK_TIMER.Value = Service.DEFAULT_RESTOCK_TIME
     task.spawn(function()
