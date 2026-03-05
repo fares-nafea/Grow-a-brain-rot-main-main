d72046176471513a9a288224e175e187305f8d0f
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-03-06 00:57:41 +0200

    Refactor SeedToolVisual script to improve code readability by standardizing attribute access and enhancing comments for clarity on tool functionality.

--- src/client/SeedToolVisual.client.lua
+++ src/client/SeedToolVisual.client.lua
@@ -1,4 +1,7 @@
 -- SeedToolVisual
+--[[
+	For displaying the player's plot when holding a tool
+]]
 
 local players = game:GetService("Players")
 local tweenService = game:GetService("TweenService")
@@ -8,46 +11,43 @@ local character = script.Parent
 
 local plots = workspace:WaitForChild("World", math.huge).Map.Plots
 
-local humanoid: Humanoid = character:WaitForChild("humanoid", math.huge)
-
+local humanoid: Humanoid = character:WaitForChild("Humanoid", math.huge)
 local highlightToUse = script:WaitForChild("Highlight", math.huge)
 
--- Defaulting Highlight
-
+-- Defaulting Highlight and Creating Tweens
 highlightToUse.FillTransparency = 1
 
 local tweenIn = tweenService:Create(highlightToUse, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {
-    FillTransparency = .5
+	FillTransparency = .5
 })
-
 local tweenOut = tweenService:Create(highlightToUse, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {
-    FillTransparency = 1
+	FillTransparency = 1
 })
 --
 
 repeat task.wait() until player:GetAttribute("DataLoaded") == true
 
 local function getPlayerPlot()
-    for _,v in plots:GetChildren()do
-        if v:IsA("Model") and v:GetAttribute("Taken") == true and v:GetAttribute("Owner") == player.UserId then
-            return v
-        end
-    end
-    return nil
+	for _,v in plots:GetChildren() do
+		if v:IsA("Model") and v:GetAttribute("Taken") == true and v:GetAttribute("Owner") == player.UserId then
+			return v
+		end
+	end
+	return nil
 end
 
 repeat task.wait() until getPlayerPlot()
 highlightToUse.Adornee = getPlayerPlot().RightSoil
 
 character.ChildAdded:Connect(function(child: Instance)
-    if child:IsA("Tool") and child:GetAttributes("isSeed") == true then
-        tweenOut:Pause()
-        tweenIn:Play()
-    end
+	if child:IsA("Tool") and child:GetAttribute("isSeed") == true then
+		tweenOut:Pause()
+		tweenIn:Play()
+	end
 end)
 character.ChildRemoved:Connect(function(child: Instance)
-    if child:IsA("Tool") and child:GetAttributes("isSeed") == true then
-        tweenIn:Pause()
-        tweenOut:Play()
-    end
+	if child:IsA("Tool") and child:GetAttribute("isSeed") == true then
+		tweenIn:Pause()
+		tweenOut:Play()
+	end
 end)
